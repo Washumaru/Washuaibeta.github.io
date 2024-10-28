024035de38677f5fa7f68baa30ee6fa1f758e9e5
Washumaru <aarontorres1405@gmail.com>
2024-10-28 01:06:12 -0600

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Washuaibeta.github.io/', // Añade esta línea con el nombre de tu repositorio
+  base: '/Washuaibeta.github.io/',
   plugins: [react()],
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    // Asegura que los assets se manejen correctamente en GitHub Pages
     sourcemap: true,
-  },
-  server: {
-    port: 3000,
-    open: true,
   }
 });
